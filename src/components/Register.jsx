bda2c6f8e8ad324b319732b03f5ef4e99e5b800a
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-03 20:24:49 +0600

    added registration form in register.jsx file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setError("");
+
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        console.log(name, photo, email, password)
+
+        if(password.length<6){
+            setError("Password must be more than 6 character");
+            return;
+        }
+
+        
+    }
     return (
-        <div>
-            This is Register
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="photo" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email"  placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered"  required/>
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <p className='text-red-500 text-center'><small>{error}</small></p>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
