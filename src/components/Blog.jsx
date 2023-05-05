5577c8fed9628205482edf86b174bd7258ae5305
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-05 20:11:55 +0600

    changed spinner and a heading to blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,39 +6,43 @@ const Blog = () => {
     console.log(Pdf);
     return (
         <div>
-            <div className='text-center my-8'>
-                <Pdf targetRef={ref} filename="div-blue.pdf">
-                    {({ toPdf }) => (
-                        <button onClick={toPdf} className="btn btn-outline btn-accent">Generate pdf</button>
-                    )}
-                </Pdf>
-            </div>
 
             <div ref={ref} className='bg-base-100 h-screen md:mx-32 p-4'>
-                                    {/* First Question */}
+                <h1 className='text-center py-12 my-4 rounded bg-black text-white font-bold text-4xl'>Question And Answer Page</h1>
+                <div className='px-8 pt-4'>
+                    {/* First Question */}
 
-                <h1 className='text-xl'>Tell us the differences between uncontrolled and controlled components.</h1>
-                <p className='p-4 bg-slate-300 rounded my-2'>Controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
-                                    
-                                    {/* Second Question */}
+                    <h1 className='text-xl'>Tell us the differences between uncontrolled and controlled components.</h1>
+                    <p className='p-4 bg-slate-300 rounded my-2'>Controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
 
-                <h1 className='text-xl'>How to validate React props using PropTypes</h1>
+                    {/* Second Question */}
 
-                <p className='p-4 bg-slate-300 rounded my-2'>PropTypes are mechanism to check if the passed value of a props is of the correct datatype. To validate props by propTypes,
-                 we have to install prop-types and import. propTypes are object with a key and a value pair where the `key` is the name of the prop while the value represent the type or class by which they are defined. They are often used after the the component end and stats with the name of the component.</p>
-                                    
-                                    {/* Third Question */}
+                    <h1 className='text-xl'>How to validate React props using PropTypes</h1>
 
-                <h1 className='text-xl'>Tell us the difference between nodejs and express js.</h1>
+                    <p className='p-4 bg-slate-300 rounded my-2'>PropTypes are mechanism to check if the passed value of a props is of the correct datatype. To validate props by propTypes,
+                        we have to install prop-types and import. propTypes are object with a key and a value pair where the `key` is the name of the prop while the value represent the type or class by which they are defined. They are often used after the the component end and stats with the name of the component.</p>
 
-                <p className='p-4 bg-slate-300 rounded my-2'>Node. js is an open-source and cross-platform that is used to execute JavaScript code outside of a browser. Express. js is used to develop complete web applications such as single-page, multi-page, and hybrid web applications and APIs.</p>
+                    {/* Third Question */}
 
-                                    {/* Fourth Question */}
-                                    
-                <h1 className='text-xl'>What is a custom hook, and why will you create a custom hook?</h1>
+                    <h1 className='text-xl'>Tell us the difference between nodejs and express js.</h1>
 
-                <p className='p-4 bg-slate-300 rounded my-2'>A custom Hook is a JavaScript function whose name starts with use keyword. Inside a custom Hook we can use other hooks also. The main reason to write a custom hook is for code reusability. It helps to keep the code clean.</p>
+                    <p className='p-4 bg-slate-300 rounded my-2'>Node. js is an open-source and cross-platform that is used to execute JavaScript code outside of a browser. Express. js is used to develop complete web applications such as single-page, multi-page, and hybrid web applications and APIs.</p>
 
+                    {/* Fourth Question */}
+
+                    <h1 className='text-xl'>What is a custom hook, and why will you create a custom hook?</h1>
+
+                    <p className='p-4 bg-slate-300 rounded my-2'>A custom Hook is a JavaScript function whose name starts with use keyword. Inside a custom Hook we can use other hooks also. The main reason to write a custom hook is for code reusability. It helps to keep the code clean.</p>
+                </div>
+
+            </div>
+            {/* Pdf generate Button */}
+            <div className='text-center my-8'>
+                <Pdf targetRef={ref} filename="qanda.pdf">
+                    {({ toPdf }) => (
+                        <button onClick={toPdf} className="btn">Generate pdf</button>
+                    )}
+                </Pdf>
             </div>
         </div>
     );
--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -1,20 +1,32 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProviders';
 import { Navigate, useLocation } from 'react-router-dom';
+import {ThreeCircles} from 'react-loader-spinner';
 
 const PrivateRoute = ({ children }) => {
 
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading) {
-        return <div className='w-screen h-36 grid place-items-center'><progress className="progress w-56"></progress></div>
+    if (loading) {
+        return <div className='w-screen h-36 grid place-items-center'><ThreeCircles
+            height="100"
+            width="100"
+            color="#4fa94d"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="three-circles-rotating"
+            outerCircleColor=""
+            innerCircleColor=""
+            middleCircleColor=""
+        /></div>
     }
     if (user) {
         return children;
     }
 
-    return <Navigate to='/login' state={{from: location}} replace></Navigate>
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
 };
 
 export default PrivateRoute;
